fd434d960688624b6039afdcf31cdde6f732705f
Daniel Boggiano <dboggiano@itdata.com.pe>
2021-05-10 19:30:35 -0500

    solor page description

--- src/app/pages/colors-page/colors-page.component.ts
+++ src/app/pages/colors-page/colors-page.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorsPageComponent implements OnInit {
   title:string = 'Color Utilities'
-  description:string = `Bootstrap's default utility classes can be found on the official <a href="https://getbootstrap.com/docs">Bootstrap Documentation</a> page. The custom utilities below were created to extend this theme past the default utility classes built into Bootstrap's framework.`
+  description:string = ``
   constructor() { }
 
   ngOnInit(): void {
